6de3260b09e6453f44a6ee46422037659e0f4f48
Soraya Rezaie <banorezaie2004@gmail.com>
2023-11-22 19:36:50 +0430

    js changes

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const submit = document.getElementById('submit');
 const p = document.getElementById('click');
 submit.addEventListener('click', function(){
     event.preventDefault();
-    p.textContent = "Hey you clicked once on me. Event type is: " + event.type;
+    p.textContent = "Hey you clicked once on me. Event type is: " + event.type + "On "+ Date();
 });
