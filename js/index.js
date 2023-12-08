e69620631d929f29a61e88c68c40bc8c5f0a1ede
Soraya Rezaie <banorezaie2004@gmail.com>
2023-12-08 17:59:49 +0430

    js changes method

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const submit = document.getElementById('submit');
 const p = document.getElementById('click');
-submit.addEventListener('click', function(){
+submit.addEventListener('click', function(event){
     event.preventDefault();
     p.textContent = "Hey you clicked once on me. Event type is: " + event.type + "On "+ Date();
 });
